6a8f5b4f1df28db5c4d988f62df7fb6093878553
Alvaro Pereyra <alvaro@xendacentral.com>
2012-01-21 09:34:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeChat::Application.config.secret_token = '2faefaadb7e56bdf126a601f43d16bd01744502da1320fe958d5a7c4bd374dfbafe39324817b46dcb2d36e005a7acfbe229503ba47a087be6a19c519fa77bd59'
